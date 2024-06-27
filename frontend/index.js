bbf06041eda52a78a4bb593c12db1d028495840c
Kenny Hung (he) <kenny.cfg@outlook.com>
2024-06-27 20:30:01 +0100

    Make HTML, and display

--- frontend/index.js
+++ frontend/index.js
@@ -1,9 +1,18 @@
 const button = document.getElementById('fetch-todos')
+const todoItemsElement = document.getElementById('todo-items')
 
 button.addEventListener('click', async () => {
+  todoItemsElement.innerHTML = ''
   const results = await fetch(
     'http://localhost:5000/item'
   );
   const jsonResult = await results.json()
-  console.log(jsonResult)
+  const list = document.createElement('ul');
+  jsonResult.forEach(todoItem => {
+    const text = todoItem.text
+    const listItem = document.createElement('li')
+    listItem.innerText = text
+    list.appendChild(listItem);
+  })
+  todoItemsElement.appendChild(list);
 })
